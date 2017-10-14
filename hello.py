e8d1d845dbece00c90b84c880412110895518a3e
AJRedDevil <ajan.shresh@gmail.com>
2017-10-14 14:40:17 +0545

    abort

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 '''Initialization'''
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, abort
 '''
 applicaton instance
 Flask uses name argument to determine the root path of the application
@@ -19,9 +19,12 @@ def index():
     return '<p>Your browser is %s</p>' % user_agent
     return '<div>Hello world!<div>'
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % (name)
+@app.route('/user/<id>')
+def user(id):
+    user = load_user(id)
+    if not user:
+        abort(400)
+    return '<h1>Hello, %s!</h1>' % (user.name)
 
 if __name__ == "__main__":
     app.run(debug=True)
