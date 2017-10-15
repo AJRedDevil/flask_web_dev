f5cc448920aa171b46615d30f69d248ed158f7e2
AJRedDevil <ajan.shresh@gmail.com>
2017-10-15 19:09:47 +0545

    Login Logout form and Protecting routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+
 from config import config
 
 boostrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong' # None | basic | strong
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,6 +24,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # attach routes and customer error pages here
 
