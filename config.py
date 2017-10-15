5d04beff3a5d4cefc9d462a35f45605458591ea6
AJRedDevil <ajan.shresh@gmail.com>
2017-10-15 19:44:07 +0545

    User Registration

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '@AJR3dD3v1l'
     SQLACHEMY_COMMIT_ON_TEARDOWN = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
