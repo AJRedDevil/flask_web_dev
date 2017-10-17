335fd9aa7c8cffe7e3c480dc70aa351c618056ad
AJRedDevil <ajan.shresh@gmail.com>
2017-10-17 07:52:56 +0545

    User Profiles

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,12 +8,13 @@ from ..models import User
 from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
     PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
 
-@auth.before_app_first_request
+@auth.before_app_request
 def before_request():
-    if current_user.is_authenticated \
-            and not current_user.confirmed \
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
             and request.endpoint[:5] != 'auth.':
-        return redirect(url_for('auth.unconfirmed'))
+            return redirect(url_for('auth.unconfirmed'))
 
 @auth.route('/unconfirmed')
 def unconfirmed():
